7b9908210d076875c00746c9eab4cb8bb80003c5
Matheus Fábio <matheusfabiors@gmail.com>
2024-12-27 14:47:53 -0300

    feat: Create the user's router

--- src/app.js
+++ src/app.js
@@ -1,18 +1,43 @@
 // const express = require("express");
 import express from "express";
-const app = express();
 
+const app = express();
+//indicar para o express ler body com json
+app.use(express.json());
 const users = [
-  { name: "John", email: "joaopep@gow.bn" },
-  { name: "Matheus", email: "gogogmig@arakfe.gm" },
-  { name: "Lucy", email: "gujuf@ho.jm" },
-  { name: "Natan", email: "enoja@talsiice.be" },
+  { userId: 1, name: "John", email: "joaopep@gow.bn" },
+  { userId: 2, name: "Matheus", email: "gogogmig@arakfe.gm" },
+  { userId: 3, name: "Lucy", email: "gujuf@ho.jm" },
+  { userId: 4, name: "Natan", email: "enoja@talsiice.be" },
 ];
+
+function findUserById(userId) {
+  return users.filter((user) => user.userId == userId);
+}
+
+function findIdUser(userId) {
+  return users.findIndex((user) => user.userId == userId);
+}
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/users", (req, res) => {
   res.status(200).send(users);
+});
+
+app.get("/users/:userId", (req, res) => {
+  res.json(findUserById(req.params.userId));
+});
+
+app.post("/users", (req, res) => {
+  users.push(req.body);
+  res.status(201).json(users);
+});
+
+app.delete("/users/:userId", (req, res) => {
+  let index = findIdUser(req.params.userId);
+  users.splice(index, 1);
+  res.send(`User id ${req.params.userId} was deleted successfully`);
 });
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import app from "./app.js";
 import { config } from "dotenv";
 
+//Call the .env file
 config();
 
+//Call the port value that are on .env file
 const port = process.env.PORT;
 
+//Show on the terminal, what is the port number that is running
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
